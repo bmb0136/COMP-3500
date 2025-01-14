bf69923ef7d7edd4e5cee61e37dc71f684618521
Brandon Buckley <brandon@wsl.>
2025-01-14 16:19:26 +0000

    Add flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      tools = with pkgs; [
+        gcc
+        gdb
+        ethtool
+        pciutils
+        ncurses
+      ];
+    in {
+      devshells.default = pkgs.mkShellNoCC {
+        packages = tools;
+      };
+    });
+}
