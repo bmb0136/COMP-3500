866aa55030488bc5f46218080cc4ad1bdcf5db92
Brandon Buckley <brandonmb0136@gmail.com>
2025-03-26 17:33:47 +0000

    Revert "Formatting"
    This reverts commit 5c6911d683c3151dde5a82e7614aa717396313db.

--- kern/asst1/catsem.c
+++ kern/asst1/catsem.c
@@ -3,24 +3,26 @@
  *
  * 30-1-2003 : GWA : Stub functions created for CS161 Asst1.
  *
- * NB: Please use SEMAPHORES to solve the cat syncronization problem in
+ * NB: Please use SEMAPHORES to solve the cat syncronization problem in 
  * this file.
  */
 
+
 /*
- *
+ * 
  * Includes
  *
  */
 
+#include <types.h>
 #include <lib.h>
-#include <synch.h>
 #include <test.h>
 #include <thread.h>
-#include <types.h>
+#include <synch.h>
+
 
 /*
- *
+ * 
  * Constants
  *
  */
@@ -57,9 +59,8 @@
 /*
  * Names of each animal
  */
-static volatile const char *catNames[NCATS] = {"Charlie", "Cooper", "Chad",
-                                               "Carson",  "Caleb",  "Claire"};
-static volatile const char *mouseNames[NMICE] = {"Mickey", "Minnie"};
+static volatile const char *catNames[NCATS] = { "Charlie", "Cooper", "Chad", "Carson", "Caleb", "Claire" };
+static volatile const char *mouseNames[NMICE] = { "Mickey", "Minnie" };
 
 /*
  *
@@ -80,66 +81,58 @@ static volatile /*bool*/ char kitchenFree = 1;
 static volatile struct semaphore *doneSem = NULL;
 static volatile /*bool*/ char run = 0;
 
+
 /*
- *
+ * 
  * Function Definitions
- *
+ * 
  */
 
 /*
  * Prototypes for animalsem()
  */
-static void tryTakeKitchen(const char *animalName, /*bool*/ char isCat,
-                           struct semaphore *queue, unsigned long *waiting);
-static void enterKitchen(const char *animalName, /*bool*/ char isCat,
-                         int animalCount, struct semaphore *queue,
+static void takeKitchen(const char *animalName, /*bool*/ char isCat,
+                        struct semaphore *queue,
+                        unsigned long *waiting);
+static void enterKitchen(const char *animalName, /*bool*/ char isCat, int animalCount,
+                         struct semaphore *queue,
                          unsigned long *waiting);
-static void leaveKitchen(const char *animalName, /*bool*/ char isCat,
-                         struct semaphore *queue, struct semaphore *otherQueue,
-                         unsigned long *waiting, unsigned long *otherWaiting);
+static void leaveKitchen(const char *animalName, /*bool*/ char isCat, struct semaphore *queue, struct semaphore *otherQueue, unsigned long *waiting, unsigned long *otherWaiting);
 
 /*
  *
  * animalsem(): Animal-agnostic Cat & Mouse implementation
  *
  * Arguments:
- *    (bool) char isCat: Is the animal calling this function a cat or a mouse?
- * True (non-zero) if cat, false (zero) if mouse unsigned long animalNumber: the
- * specific animal that is calling this function unsigned long animalCount: the
- * total number of animals of the caller's type struct semaphore *queue: the
- * queue for animals of the caller's type struct semaphore *otherQueue: the
- * queue for animals NOT of the caller's type unsigned long *waiting: the number
- * of animals of the caller's type unsigned long *otherWaiting: the number of
- * animals NOT of the caller's type
+ *    (bool) char isCat: Is the animal calling this function a cat or a mouse? True (non-zero) if cat, false (zero) if mouse
+ *    unsigned long animalNumber: the specific animal that is calling this function
+ *    unsigned long animalCount: the total number of animals of the caller's type
+ *    struct semaphore *queue: the queue for animals of the caller's type
+ *    struct semaphore *otherQueue: the queue for animals NOT of the caller's type
+ *    unsigned long *waiting: the number of animals of the caller's type
+ *    unsigned long *otherWaiting: the number of animals NOT of the caller's type
  *
  */
-static void animalsem(/*bool*/ char isCat, unsigned long animalNumber,
-                      int animalCount, struct semaphore *queue,
-                      struct semaphore *otherQueue, unsigned long *waiting,
-                      unsigned long *otherWaiting) {
+static
+void
+animalsem(/*bool*/ char isCat, unsigned long animalNumber, int animalCount,
+          struct semaphore *queue, struct semaphore *otherQueue,
+          unsigned long *waiting, unsigned long *otherWaiting)
+{
 
   while (!run) {
     thread_yield();
   }
 
-  const char *animalName =
-      isCat ? catNames[animalNumber] : mouseNames[animalNumber];
+  const char *animalName = isCat ? catNames[animalNumber] : mouseNames[animalNumber];
   kprintf("!!! %s Started\n", animalName);
 
   while (run) {
-    tryTakeKitchen(animalName, isCat, queue, waiting);
-
-    // Wait to be let in
+    takeKitchen(animalName, isCat, queue, waiting);
     P(queue);
-
     enterKitchen(animalName, isCat, animalCount, queue, waiting);
-
-    kprintf("... %s Eating\n", animalName);
     clocksleep(random() % MAXTIME);
-
     leaveKitchen(animalName, isCat, queue, otherQueue, waiting, otherWaiting);
-
-    kprintf("... %s Playing\n", animalName);
     clocksleep(random() % MAXTIME);
   }
   kprintf("!!! %s Exited\n", animalName);
@@ -147,29 +140,28 @@ static void animalsem(/*bool*/ char isCat, unsigned long animalNumber,
 }
 
 /*
- * tryTakeKitchen(): Let somebody in if the kitchen is free. Handles the case
- * when no animals are in the kitchen (which is the case initially).
+ * takeKitchen(): Let somebody in if the kitchen is free. Handles the case when no animals are in the kitchen (which is the case initially).
  *
  * Arguments:
  *    const char *animalName: The name of the animal that called this function
  *    (bool) char isCat: Whether the caller is a cat or not
  *    struct semaphore *queue: The queue to signal animals of the caller's type
- *    unsigned long *waiting: The number of animals of the caller's type that
- * are waiting to enter the kitchen
+ *    unsigned long *waiting: The number of animals of the caller's type that are waiting to enter the kitchen
  */
-static void tryTakeKitchen(const char *animalName, /*bool*/ char isCat,
-                           struct semaphore *queue, unsigned long *waiting) {
+static
+void
+takeKitchen(const char *animalName, /*bool*/ char isCat,
+            struct semaphore *queue,
+            unsigned long *waiting)
+{
   P(mutex);
-
   if (kitchenFree) {
     kitchenFree = 0;
-    kprintf("+++ %s Letting another %s in (kitchen free)\n", animalName,
-            isCat ? "Cat" : "Mouse");
+    kprintf("+++ %s Letting another %s in (kitchen free)\n", animalName, isCat ? "Cat" : "Mouse");
     V(queue);
   }
   (*waiting)++;
   kprintf("*** %s Hungry\n", animalName);
-
   V(mutex);
 }
 
@@ -181,71 +173,63 @@ static void tryTakeKitchen(const char *animalName, /*bool*/ char isCat,
  *    (bool) char isCat: Whether the caller is a cat or not
  *    int animalCount: The total number of animals of the caller's type
  *    struct semaphore *queue: The queue to signal animals of the caller's type
- *    unsigned long *waiting: The number of animals of the caller's type that
- * are waiting to enter the kitchen
+ *    unsigned long *waiting: The number of animals of the caller's type that are waiting to enter the kitchen
  */
-static void enterKitchen(const char *animalName, /*bool*/ char isCat,
-                         int animalCount, struct semaphore *queue,
-                         unsigned long *waiting) {
-  P(mutex);
-
-  kprintf(">>> %s Entered the kitchen (%d/%d dishes used)\n", animalName,
-          dishesUsed + 1, NFOODBOWLS);
-  dishesUsed++;
-  (*waiting)--;
-
-  // This ternary is here to handle the possibility of having more bowls than
-  // animals of a specific type.
-  int maxAnimalsToLetIn = animalCount < NFOODBOWLS ? animalCount : NFOODBOWLS;
-  if ((*waiting) && dishesUsed < maxAnimalsToLetIn) {
-    kprintf("+++ %s Letting another %s in (dishes available)\n", animalName,
-            isCat ? "Cat" : "Mouse");
-    V(queue);
-  }
-
-  V(mutex);
+static
+void
+enterKitchen(const char *animalName, /*bool*/ char isCat, int animalCount,
+                         struct semaphore *queue,
+                         unsigned long *waiting)
+{
+    P(mutex);
+    kprintf(">>> %s Entered the kitchen\n", animalName);
+    dishesUsed++;
+    (*waiting)--;
+    if ((*waiting) && dishesUsed < (animalCount < NFOODBOWLS ? animalCount : NFOODBOWLS)) {
+      kprintf("+++ %s Letting another %s in (dishes available)\n", animalName, isCat ? "Cat" : "Mouse");
+      V(queue);
+    }
+    kprintf("... %s Eating\n", animalName);
+    V(mutex);
 }
 
 /*
- * leaveKitchen(): Leaves the kitchen. Will let the opposite animal type in if
- * possible, then try its own type, and finally will free the kitchen otherwise.
+ * leaveKitchen(): Leaves the kitchen. Will let the opposite animal type in if possible, then try its own type, and finally will free the kitchen otherwise.
  *
  * Arguments:
  *    const char *animalName: The name of the animal that called this function
  *    (bool) char isCat: Whether the caller is a cat or not
  *    struct semaphore *queue: The queue to signal animals of the caller's type
- *    struct semaphore *otherQueue: The queue to signal animals NOT of the
- * caller's type unsigned long *waiting: The number of animals of the caller's
- * type that are waiting to enter the kitchen unsigned long *otherWaiting: The
- * number of animals NOT of the caller's type that are waiting to enter the
- * kitchen
+ *    struct semaphore *otherQueue: The queue to signal animals NOT of the caller's type
+ *    unsigned long *waiting: The number of animals of the caller's type that are waiting to enter the kitchen
+ *    unsigned long *otherWaiting: The number of animals NOT of the caller's type that are waiting to enter the kitchen
  */
-static void leaveKitchen(const char *animalName, /*bool*/ char isCat,
-                         struct semaphore *queue, struct semaphore *otherQueue,
-                         unsigned long *waiting, unsigned long *otherWaiting) {
+static
+void
+leaveKitchen(const char *animalName, /*bool*/ char isCat,
+             struct semaphore *queue, struct semaphore *otherQueue,
+             unsigned long *waiting, unsigned long *otherWaiting)
+{
   P(mutex);
-
-  kprintf("<<< %s Left the kitchen (%d/%d dishes used)\n", animalName,
-          dishesUsed - 1, NFOODBOWLS);
+  kprintf("<<< %s Left the kitchen\n", animalName);
   dishesUsed--;
   if (dishesUsed == 0) {
     if (*otherWaiting > 0) {
       kprintf("+++ %s Letting a %s in\n", animalName, isCat ? "Mouse" : "Cat");
       V(otherQueue);
     } else if (*waiting > 0) {
-      kprintf("+++ %s Letting another %s in (after leaving)\n", animalName,
-              isCat ? "Cat" : "Mouse");
+      kprintf("+++ %s Letting another %s in (after leaving)\n", animalName, isCat ? "Cat" : "Mouse");
       V(queue);
     } else {
       kprintf("--- %s Freeing kitchen\n", animalName);
       kitchenFree = 1;
     }
   }
-
+  kprintf("... %s Playing\n", animalName);
   V(mutex);
 }
 
-/*
+/*stuff
  * catsem()
  *
  * Arguments:
@@ -260,18 +244,24 @@ static void leaveKitchen(const char *animalName, /*bool*/ char isCat,
  *
  */
 
-static void catsem(void *unusedpointer, unsigned long catnumber) {
-  (void)unusedpointer;
-  animalsem(1, catnumber, NCATS, catsQueue, miceQueue, &catsWaiting,
-            &miceWaiting);
+static
+void
+catsem(void * unusedpointer, 
+       unsigned long catnumber)
+{
+        (void) unusedpointer;
+        animalsem(1, catnumber, NCATS,
+                  catsQueue, miceQueue,
+                  &catsWaiting, &miceWaiting);
 }
+        
 
 /*
  * mousesem()
  *
  * Arguments:
  *      void * unusedpointer: currently unused.
- *      unsigned long mousenumber: holds the mouse identifier from 0 to
+ *      unsigned long mousenumber: holds the mouse identifier from 0 to 
  *              NMICE - 1.
  *
  * Returns:
@@ -282,11 +272,17 @@ static void catsem(void *unusedpointer, unsigned long catnumber) {
  *
  */
 
-static void mousesem(void *unusedpointer, unsigned long mousenumber) {
-  (void)unusedpointer;
-  animalsem(0, mousenumber, NMICE, miceQueue, catsQueue, &miceWaiting,
-            &catsWaiting);
+static
+void
+mousesem(void * unusedpointer, 
+         unsigned long mousenumber)
+{
+        (void) unusedpointer;
+        animalsem(0, mousenumber, NMICE,
+                  miceQueue, catsQueue,
+                  &miceWaiting, &catsWaiting);
 }
+
 
 /*
  * catmousesem()
@@ -299,97 +295,116 @@ static void mousesem(void *unusedpointer, unsigned long mousenumber) {
  *      0 on success.
  *
  * Notes:
- *      Driver code to start up catsem() and mousesem() threads.  Change this
+ *      Driver code to start up catsem() and mousesem() threads.  Change this 
  *      code as necessary for your solution.
  */
 
-int catmousesem(int nargs, char **args) {
-  int index, error;
+int
+catmousesem(int nargs,
+            char ** args)
+{
+        int index, error;
+   
+        /*
+         * Avoid unused variable warnings.
+         */
 
-  /*
-   * Avoid unused variable warnings.
-   */
+        (void) nargs;
+        (void) args;
 
-  (void)nargs;
-  (void)args;
+        /*
+         * Init shared variables
+         */
+        catsQueue = sem_create("Cats Queue", 0);
+        if (!catsQueue) {
+          panic("catmousesem: Failed to create cats queue\n");
+        }
+        miceQueue = sem_create("Mice Queue", 0);
+        if (!miceQueue) {
+          panic("catmousesem: Failed to create mice queue\n");
+        }
+        mutex = sem_create("Cats and Mice Mutex", 1);
+        if (!mutex) {
+          panic("catmousesem: Failed to create mutex\n");
+        }
+        doneSem = sem_create("Thread Exit Waiter", 0);
+        if (!doneSem) {
+          panic("catmousesem: Failed to done semaphore\n");
+        }
+   
+        /*
+         * Start NCATS catsem() threads.
+         */
 
-  /*
-   * Init shared variables
-   */
-  catsQueue = sem_create("Cats Queue", 0);
-  if (!catsQueue) {
-    panic("catmousesem: Failed to create cats queue\n");
-  }
-  miceQueue = sem_create("Mice Queue", 0);
-  if (!miceQueue) {
-    panic("catmousesem: Failed to create mice queue\n");
-  }
-  mutex = sem_create("Cats and Mice Mutex", 1);
-  if (!mutex) {
-    panic("catmousesem: Failed to create mutex\n");
-  }
-  doneSem = sem_create("Thread Exit Waiter", 0);
-  if (!doneSem) {
-    panic("catmousesem: Failed to done semaphore\n");
-  }
+        for (index = 0; index < NCATS; index++) {
+           
+                error = thread_fork("catsem Thread", 
+                                    NULL, 
+                                    index, 
+                                    catsem, 
+                                    NULL
+                                    );
+                
+                /*
+                 * panic() on error.
+                 */
 
-  /*
-   * Start NCATS catsem() threads.
-   */
+                if (error) {
+                 
+                        panic("catsem: thread_fork failed: %s\n", 
+                              strerror(error)
+                              );
+                }
+        }
+        
+        /*
+         * Start NMICE mousesem() threads.
+         */
 
-  for (index = 0; index < NCATS; index++) {
+        for (index = 0; index < NMICE; index++) {
+   
+                error = thread_fork("mousesem Thread", 
+                                    NULL, 
+                                    index, 
+                                    mousesem, 
+                                    NULL
+                                    );
+                
+                /*
+                 * panic() on error.
+                 */
 
-    error = thread_fork("catsem Thread", NULL, index, catsem, NULL);
+                if (error) {
+         
+                        panic("mousesem: thread_fork failed: %s\n", 
+                              strerror(error)
+                              );
+                }
+        }
 
-    /*
-     * panic() on error.
-     */
+  
+        /*
+         * Run simulation
+         */
+        kprintf("### Driver: Starting simulation\n");
+        run = 1;
+        clocksleep(RUNTIME);
+        kprintf("### Driver: Ending simulation\n");
+        run = 0;
 
-    if (error) {
+        /*
+         * Wait for all animals to finish
+         */
+        kprintf("### Driver: Waiting for threads to exit\n");
+        run = 0;
+        for (index = 0; index < (NCATS + NMICE); index++) {
+                P(doneSem);
+        }
+        kprintf("### Driver: done\n");
 
-      panic("catsem: thread_fork failed: %s\n", strerror(error));
-    }
-  }
-
-  /*
-   * Start NMICE mousesem() threads.
-   */
-
-  for (index = 0; index < NMICE; index++) {
-
-    error = thread_fork("mousesem Thread", NULL, index, mousesem, NULL);
-
-    /*
-     * panic() on error.
-     */
-
-    if (error) {
-
-      panic("mousesem: thread_fork failed: %s\n", strerror(error));
-    }
-  }
-
-  /*
-   * Run simulation
-   */
-  kprintf("### Driver: Starting simulation\n");
-  run = 1;
-  clocksleep(RUNTIME);
-  kprintf("### Driver: Ending simulation\n");
-  run = 0;
-
-  /*
-   * Wait for all animals to finish
-   */
-  kprintf("### Driver: Waiting for threads to exit\n");
-  run = 0;
-  for (index = 0; index < (NCATS + NMICE); index++) {
-    P(doneSem);
-  }
-  kprintf("### Driver: done\n");
-
-  return 0;
+        return 0;
 }
+
 
 /*
  * End of catsem.c
